fbdb82edf89268202bff089794f680edf81068c6
chhavi pareek <chhavippareek@gmail.com>
2024-07-20 12:40:00 +0530

    strength

--- Password_strength.py
+++ Password_strength.py
@@ -1,39 +1,40 @@
 import re
 
-def is_strong_password(password):
+def assess_password_strength(password):
+    feedback = []
+
     # Minimum length of 8 characters
     if len(password) < 8:
-        return False
+        feedback.append("Password should be at least 8 characters long.")
     
-    # Should contain at least one lowercase letter
-    if not any(char.islower() for char in password):
-        return False
+    # Should contain at least 2 uppercase letters
+    if sum(1 for char in password if char.isupper()) < 2:
+        feedback.append("Password should contain at least 2 uppercase letters.")
     
-    # Should contain at least one uppercase letter
-    if not any(char.isupper() for char in password):
-        return False
-    
-    # Should contain at least one digit
-    if not any(char.isdigit() for char in password):
-        return False
+    # Should contain at least 4 digits
+    if sum(1 for char in password if char.isdigit()) < 4:
+        feedback.append("Password should contain at least 4 digits.")
     
     # Should contain at least one special character
     special_characters = "!@#$%^&*()-=_+[]{}|;:'\",.<>/?"
     if not any(char in special_characters for char in password):
-        return False
+        feedback.append(f"Password should contain at least one special character from: {special_characters}")
     
     # Should not contain spaces
     if ' ' in password:
-        return False
+        feedback.append("Password should not contain spaces.")
     
-    # If all criteria are met, the password is considered strong
-    return True
+    if feedback:
+        return False, feedback
+    else:
+        return True, ["Password is strong!"]
 
 # Get password from the user
 password = input("Enter your password: ")
 
 # Check password strength
-if is_strong_password(password):
-    print("Password is strong!")
-else:
-    print("Password is weak. Please follow the password requirements.")
+is_strong, messages = assess_password_strength(password)
+
+# Provide feedback to the user
+for message in messages:
+    print(message)
